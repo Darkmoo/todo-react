b0f8cd440f9a0173652c2754c75e97c2dd131a14
Darkmoo <multimadier16@yandex.ru>
2022-02-01 16:02:37 +0300

    Updates

---
+++ precache-manifest.86b4336400fbc51bca2d80c1f3b453d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66a61cbd2ffd15b1bc5e4c5ae3d6c949",
+    "url": "./index.html"
+  },
+  {
+    "revision": "216a6e9ee492c030089c",
+    "url": "./static/css/main.f4903a8f.chunk.css"
+  },
+  {
+    "revision": "f09b246faf15a1ff0037",
+    "url": "./static/js/2.85a581b9.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.85a581b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "216a6e9ee492c030089c",
+    "url": "./static/js/main.cdb8e375.chunk.js"
+  },
+  {
+    "revision": "d3b36346763b48e2e02c",
+    "url": "./static/js/runtime-main.e63fe8d2.js"
+  }
+]);
